684e1decff316a153b9c87ff47fb39b6e124ba5e
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-05 21:10:29 +0530

    declare_variables

---
+++ declare_variables.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main(){
+	var x int =10 //type is integer
+	var y=12 //type is inferred as integer
+	z:=13 //short hand declaration or type is inferred
+	var a string ="Lilith"
+	var b="kumar"
+
+	c:="kannan"
+	d:="rani"
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+    fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
