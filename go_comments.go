8c41c58dc8a8ec6254a2c1b10db05f1f2cea9dad
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-05 20:52:37 +0530

    go comments

---
+++ go_comments.go
@@ -0,0 +1,10 @@
+package main //declare package main
+import "fmt" //import fmt for printing output
+func main(){
+	// double slash is used for single line comment
+	/* slash asterisk is used for multi line comments 
+	and closed with asterisk slash */
+	fmt.Println("comments are generally used for read purpose where the other programmer can be easily understand what the user had done")
+	fmt.Println("single line comments has mentioned above")
+	fmt.Println("multiline comments has mentioned above")
+}
