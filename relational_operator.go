e8e4524de6ce4686144c37b58b61787f2a95e6f3
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-06 14:04:02 +0530

    relational operator

---
+++ relational_operator.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("relational operator")
+	a:=12
+	b:=13
+	fmt.Println("a>b",a>b)
+	fmt.Println("a<b",a<b)
+	fmt.Println("a==b",a==b)
+	fmt.Println("a!=b",a!=b)
+	fmt.Println("a<=b",a<=b)
+	fmt.Println("a>=b",a>=b)
+	
+}
