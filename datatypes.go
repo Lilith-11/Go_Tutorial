d4d30cf3e4fea721872e0aba2599dd12795fd62c
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-06 11:24:35 +0530

    datatypes

---
+++ datatypes.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("integer")
+	var a int =1
+	var b int8=87
+	fmt.Println(a,b)
+	fmt.Println("floating number")
+	var c float32=12.34
+	var d float64=12.2345
+	fmt.Println(c,d)
+	fmt.Println("Boolian number")
+	var istrue bool=true    
+	var isfalse bool =false
+	fmt.Println("istrue",istrue)
+	fmt.Println("isfalse",isfalse)
+	fmt.Println("strings")
+	var string1 string="hello,World!!"
+	fmt.Println(string1)
+	
+}
