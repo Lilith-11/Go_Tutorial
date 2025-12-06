42725f08a035739a851f18f3090ea0993d4ec8e6
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-06 14:18:47 +0530

    logical_operator

---
+++ logical_operator.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("logical_operator")
+	a:=true 
+	b:=false
+    fmt.Println("a && b",a&&b)
+    fmt.Println("a || b",a||b)
+    fmt.Println("!a",!a)}
