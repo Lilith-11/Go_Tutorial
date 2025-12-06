71de6e879fc1ba809dc1c3fc69f40fd1130c878d
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-06 17:57:18 +0530

    defer in go

--- Defer.go
+++ Defer.go
@@ -3,4 +3,5 @@ import "fmt"
 func main(){
 	defer fmt.Println("this is performed last")
 	fmt.Println("this is performed first")
+	//defer statement is executed in last after the main function body execution
 }
