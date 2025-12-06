4869d234556ca14e76b197db141d69f06de83b1e
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-06 13:58:08 +0530

    arithmetic operator

---
+++ arithmetic_operator.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("arthmetic operator")
+	a:=24
+	b:=2
+	fmt.Println("addition",a+b)
+	fmt.Println("subraction",a-b)
+	fmt.Println("multiplicaton",a*b)
+	fmt.Println("division",a/b)
+	fmt.Println("modulus",a%b)
+}
